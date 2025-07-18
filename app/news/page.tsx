1dbc975470486d56ae3ee6eb5b2fc72f060ddd65
Do Nguyen Tung Duong <furyghost07@gmail.com>
2025-07-18 10:42:48 +0700

    update page router

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -8,9 +8,13 @@ import React, { useState } from "react";
 import postVN from "@/data/news/vietnamese.json";
 import postEN from "@/data/news/english.json";
 import LanguageChange from "@/components/language-button";
+import { useParams } from "next/navigation";
 
 
 const NewsPage = () => {
+    const searchParams = typeof window !== "undefined" ? new URLSearchParams(window.location.search) : null;
+    const category = searchParams ? searchParams.get("category") : "";
+
     const savedLanguage = typeof window !== "undefined" && localStorage.getItem("language")
         ? localStorage.getItem("language")
         : "en";
@@ -63,6 +67,7 @@ const NewsPage = () => {
             }
         };
         updateData();
+        console.log(category)
     }, [savedLanguage]);
     return (
         <div className="flex flex-col items-center justify-start min-h-screen">
@@ -74,12 +79,24 @@ const NewsPage = () => {
                     <Image src="https://res.cloudinary.com/drkr1i9yz/image/upload/v1752805060/oh__img93_yyltcz.jpg" alt="News Banner" width={1920} height={500} className="object-cover w-full h-full" />
                 </div>
                 <h1 className="absolute top-[70%] md:top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl md:text-5xl font-bold z-20">
-                    News
+                    {
+                        category ? `${category.charAt(0).toUpperCase() + category.slice(1)}` : "News"
+                    }
                 </h1>
             </div>
             <div className="w-full mx-auto max-w-[1200px] grid grid-cols-1 lg:grid-cols-4 gap-2 md:gap-4 px-2 md:px-10 py-8">
                 {
-                    (Postdata).map((item,index) => (
+                    (Postdata.filter(
+                        item => {
+                            let categoryStr = "";
+                            if (Array.isArray(category)) {
+                                categoryStr = category.join(" ").toLowerCase();
+                            } else if (typeof category === "string") {
+                                categoryStr = category.toLowerCase();
+                            }
+                            return category ? item.category.some(cat => cat.title.toLowerCase() === categoryStr) : true;
+                        }
+                    )).map((item,index) => (
                         <PostCard
                             key={index}
                             id={item.id}
